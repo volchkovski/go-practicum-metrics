c2726bc81b44ec3886c6dcf03da5450dee078a84
Boris Volchkov <volchkovski@gmail.com>
2025-03-23 16:56:11 +0300

    changed url path check

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,43 +4,45 @@ import (
 	"fmt"
 	"github.com/volchkovski/go-practicum-metrics/internal/storage"
 	"net/http"
-	"regexp"
+	"net/url"
 	"strconv"
+	"strings"
 )
 
+func splitURLPath(u *url.URL) []string {
+	p := strings.Trim(u.Path, "/")
+	return strings.Split(p, "/")
+}
+
 func CollectMetricHandler(s storage.Storage) http.Handler {
-	re := regexp.MustCompile(`/?update/([a-zA-Z]+)/([a-zA-Z]+)/(\d+(\.\d+)?)`)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Allowed only Post method", http.StatusMethodNotAllowed)
 			return
 		}
-		result := re.FindAllStringSubmatch(r.URL.Path, -1)
-		if result == nil {
-			http.Error(w, "Invalid path to push metric", http.StatusBadRequest)
-			return
-		}
-		// [["/update/adfa/someMetric/527.00" "adfa" "someMetric" "527.00" ".00"]]
-		matches := result[0]
-		if matches[2] == "" {
+		// example: update/gauge/someMetric/527
+		parts := splitURLPath(r.URL)
+		if len(parts) < 3 {
+			// no metric name
 			http.NotFound(w, r)
 			return
 		}
-		switch matches[1] {
+		t, nm, val := parts[1], parts[2], parts[3]
+		switch t {
 		case storage.Gauge:
-			v, err := strconv.ParseFloat(matches[3], 64)
+			v, err := strconv.ParseFloat(val, 64)
 			if err != nil {
 				http.Error(w, "Value for gauge type must be float", http.StatusBadRequest)
 				return
 			}
-			s.WriteGauge(matches[2], v)
+			s.WriteGauge(nm, v)
 		case storage.Counter:
-			v, err := strconv.ParseInt(matches[3], 10, 64)
+			v, err := strconv.ParseInt(val, 10, 64)
 			if err != nil {
 				http.Error(w, "Value for counter type must be integer", http.StatusBadRequest)
 				return
 			}
-			s.WriteCounter(matches[2], v)
+			s.WriteCounter(nm, v)
 		default:
 			msg := fmt.Sprintf("Allow metric types: %s, %s", storage.Gauge, storage.Counter)
 			http.Error(w, msg, http.StatusBadRequest)
