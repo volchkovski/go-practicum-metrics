a590ffd1d24589e89154fa785bd44a40ab42bf7f
Boris Volchkov <volchkovski@gmail.com>
2025-04-13 18:50:06 +0300

    Added zap sugar logger

--- go.mod
+++ go.mod
@@ -7,10 +7,12 @@ require (
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/stretchr/testify v1.10.0
 	go.uber.org/mock v0.5.0
+	go.uber.org/zap v1.27.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,15 +1,22 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/volchkovski/go-practicum-metrics/internal/configs"
+	"github.com/volchkovski/go-practicum-metrics/internal/logger"
 	"github.com/volchkovski/go-practicum-metrics/internal/routers"
 	"github.com/volchkovski/go-practicum-metrics/internal/services"
 	"github.com/volchkovski/go-practicum-metrics/internal/storage"
 )
 
 func Run(cfg *configs.ServerConfig) error {
+	if err := logger.Initialize("debug"); err != nil {
+		return fmt.Errorf("failed to intizalize logger: %w", err)
+	}
+	defer logger.Log.Sync()
+
 	storage := storage.NewMemStorage()
 
 	service := services.NewMetricService(storage)
